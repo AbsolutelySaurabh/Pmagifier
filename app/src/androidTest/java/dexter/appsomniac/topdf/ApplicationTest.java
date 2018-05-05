96e0b9ff47e664fc67ab65ae3ec5fe19a4946c18
absolutelysaurabh <absolutelysaurabh03@gmail.com>
2018-05-06 03:03:01 +0530

    initial commit

---
+++ app/src/androidTest/java/dexter/appsomniac/topdf/ApplicationTest.java
@@ -0,0 +1,13 @@
+package dexter.appsomniac.topdf;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/dexter/appsomniac/topdf/util/ViewFilesDividerItemDecoration.java
@@ -0,0 +1,36 @@
+package dexter.appsomniac.topdf.util;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import dexter.appsomniac.topdf.R;
+
+public class ViewFilesDividerItemDecoration extends RecyclerView.ItemDecoration {
+    private Drawable mDivider;
+
+    public ViewFilesDividerItemDecoration(Context context) {
+        mDivider = context.getResources().getDrawable(R.drawable.files_divider);
+    }
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = parent.getChildAt(i);
+
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+            int top = child.getBottom() + params.bottomMargin;
+            int bottom = top + mDivider.getIntrinsicHeight();
+
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
+    }
+}
